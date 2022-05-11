c9d47a749f98a34851d116541b661c89709d2fc7
Wei Wang <89855942+YB-WangTx@users.noreply.github.com>
2022-05-11 12:16:25 -0500

    Update generate_data.sql

--- generate_data.sql
+++ generate_data.sql
@@ -12,5 +12,10 @@ values(3000, 'x3000', 'test3000', 'test3000@gmail.com', '{"Account_documents":{"
   "Account_and_service_communications":{"Account activity and service emails":"opt-in"},"Subscriptions":{"Viewpoints_Weekly_Edition":"opt-in", "Trading_and_Investing_Webinars":"opt-out"}}','US-WEST-2');
   
   
+  select * from User_Preferences_us_east;
+  
+  select * from User_Preferences_ap_southeast;
+  
+  select * from User_Preferences_us_west;
   
   select name, account_id, communication->'Subscriptions'->'Viewpoints_Weekly_Edition', communication->'Account_documents'->'Statements' from user_preferences;
