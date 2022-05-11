1973669ba7b2947cbf63de15fe482bc95f80b990
Wei Wang <89855942+YB-WangTx@users.noreply.github.com>
2022-05-11 12:32:13 -0500

    Update generate_data.sql

--- generate_data.sql
+++ generate_data.sql
@@ -11,7 +11,20 @@ values(2000, 'x2000', 'test2000', 'test2000@gmail.com', '{"Account_documents":{"
 values(3000, 'x3000', 'test3000', 'test3000@gmail.com', '{"Account_documents":{"Statements":"eDelivery", "Trade_confirmations":"eDelivery"},
   "Account_and_service_communications":{"Account activity and service emails":"opt-in"},"Subscriptions":{"Viewpoints_Weekly_Edition":"opt-in", "Trading_and_Investing_Webinars":"opt-out"}}','US-WEST-2');
   
+  --distributed transactions
+  SET force_global_transaction = TRUE;
+  BEGIN;
+   insert into user_preferences (user_id, account_id,name,contact_email,communication,preferred_region)
+   values(1001, 'x1001', 'test1001', 'test1001@gmail.com', '{"Account_documents":{"Statements":"eDelivery", "Trade_confirmations":"eDelivery"},
+  "Account_and_service_communications":{"Account activity and service emails":"opt-in"},"Subscriptions":{"Viewpoints_Weekly_Edition":"opt-in", "Trading_and_Investing_Webinars":"opt-out"}}','US-EAST-2');
   
+  insert into user_preferences (user_id, account_id,name,contact_email,communication,preferred_region)
+  values(2001, 'x2001', 'test2001', 'test2001@gmail.com', '{"Account_documents":{"Statements":"eDelivery", "Trade_confirmations":"eDelivery"},
+  "Account_and_service_communications":{"Account activity and service emails":"opt-in"},"Subscriptions":{"Viewpoints_Weekly_Edition":"opt-in", "Trading_and_Investing_Webinars":"opt-out"}}','AP-SOUTHEAST-1');
+  
+  COMMIT;
+
+---
   select * from User_Preferences_us_east;
   
   select * from User_Preferences_ap_southeast;
