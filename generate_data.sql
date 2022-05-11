d733771e577a3035a79ee847a6429e5e193dbcbc
Wei Wang <89855942+YB-WangTx@users.noreply.github.com>
2022-05-11 09:45:41 -0500

    Update generate_data.sql

--- generate_data.sql
+++ generate_data.sql
@@ -1,9 +1,6 @@
 insert into user_preferences (user_id, account_id,name,contact_email,communication,preferred_region)
 values(1000, 'x1000', 'test1', 'test1@gmail.com', '{"Account_documents":{"Statements":"eDelivery", "Trade_confirmations":"eDelivery"},
-  "Account_and_service_communications":{"Account activity and service emails":"opt-in"},"Subscriptions":{"Viewpoints_Weekly_Edition":"opt-in", "Trading_and_Investing_Webinars":"opt-out"}}','US-EAST');
+  "Account_and_service_communications":{"Account activity and service emails":"opt-in"},"Subscriptions":{"Viewpoints_Weekly_Edition":"opt-in", "Trading_and_Investing_Webinars":"opt-out"}}','US-EAST-2');
   
   
-  create table test (id int, communication jsonb)
-  
-  insert into test values (1000, '{"Account_documents":{"Statements":"eDelivery", "Trade_confirmations":"eDelivery"},
-  "Account_and_service_communications":{"Account_activity_and_service emails":"opt-in"},"Subscriptions":{"Viewpoints_Weekly_Edition":"opt-in", "Trading_and_Investing_Webinars":"opt-out"}}')
+  select name, account_id, communication->'Subscriptions'->'Viewpoints_Weekly_Edition', communication->'Account_documents'->'Statements' from user_preferences;
