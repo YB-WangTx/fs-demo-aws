fefcf3393244ed465f84b2a226227e2b0be83ea4
Wei Wang <89855942+YB-WangTx@users.noreply.github.com>
2022-05-17 13:44:18 -0500

    Update schema-temp.sql

--- schema-temp.sql
+++ schema-temp.sql
@@ -3,13 +3,15 @@
 create sequence s_user_id;
 drop table User_Preferences cascade;
 CREATE TABLE User_Preferences (
-     user_id integer DEFAULT nextval('s_user_id'),
+     customer_id integer DEFAULT nextval('s_user_id'),
+     customer_name  varchar(50),
      account_id varchar(50),
-     name varchar(50),
      contact_email varchar(50),
-     account_statement_delivery varchar(50) DEFAULT ('eDelivery'),
-     sub_view_points varchar(50)  DEFAULT ('Opt-in'),
-     preferred_region varchar(50),
+     account_statement_delivery varchar(20) DEFAULT ('US MAIL'),
+     tax_forms_delivery  varchar(20) DEFAULT ('eDelivery'),
+     trade_confirmation  varchar(20) DEFAULT ('eDelivery'),
+     Trade_education_blog varchar(20)  DEFAULT ('Opt-in'),
+     preferred_region varchar(20),
      created_date TIMESTAMP DEFAULT NOW(),
      updated_date TIMESTAMP DEFAULT NOW()
 ) PARTITION BY LIST (preferred_region);
@@ -40,28 +42,28 @@ CREATE TABLESPACE ap_southeast_1_tablespace WITH (
 --create tables
 CREATE TABLE User_Preferences_eu_central
     PARTITION OF User_Preferences
-      (user_id, name, contact_email, account_statement_delivery, sub_view_points, preferred_region,
-      created_date, updated_date, 
-      PRIMARY KEY (user_id HASH, account_id))
+      (customer_id, customer_name, account_id, contact_email, account_statement_delivery, tax_forms_delivery,trade_confirmation, 
+       trade_education_blog, preferred_region,created_date, updated_date, 
+      PRIMARY KEY (customer_id HASH, account_id))
     FOR VALUES IN ('EU') TABLESPACE eu_central_1_tablespace;
 
-CREATE INDEX ON User_Preferences_us_east(user_id) TABLESPACE eu_central_1_tablespace;
+CREATE INDEX ON User_Preferences_us_east(customer_id) TABLESPACE eu_central_1_tablespace;
 
 
 CREATE TABLE User_Preferences_us_west
     PARTITION OF User_Preferences
-      (user_id, name, contact_email, account_statement_delivery, sub_view_points, preferred_region,
-      created_date, updated_date, 
-       PRIMARY KEY (user_id HASH, account_id))
+       (customer_id, customer_name, account_id, contact_email, account_statement_delivery, tax_forms_delivery,trade_confirmation, 
+       trade_education_blog, preferred_region,created_date, updated_date, 
+       PRIMARY KEY (customer_id HASH, account_id))
     FOR VALUES IN ('US') TABLESPACE us_west_2_tablespace;
 
-CREATE INDEX ON User_Preferences_us_west(user_id) TABLESPACE us_west_2_tablespace;
+CREATE INDEX ON User_Preferences_us_west(customer_id) TABLESPACE us_west_2_tablespace;
 
 CREATE TABLE User_Preferences_ap_southeast
     PARTITION OF User_Preferences
       (user_id, name, contact_email, account_statement_delivery, sub_view_points, preferred_region,
       created_date, updated_date, 
        PRIMARY KEY (user_id HASH, account_id))
-    FOR VALUES IN ('Asian') TABLESPACE ap_southeast_1_tablespace;
+    FOR VALUES IN ('AP') TABLESPACE ap_southeast_1_tablespace;
 
-CREATE INDEX ON User_Preferences_ap_southeast(user_id) TABLESPACE ap_southeast_1_tablespace;
+CREATE INDEX ON User_Preferences_ap_southeast(customer_id) TABLESPACE ap_southeast_1_tablespace;
