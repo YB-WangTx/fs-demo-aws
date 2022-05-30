a32355868d6f8a897418ce0f25359f5d7ea2e566
Wei Wang <89855942+YB-WangTx@users.noreply.github.com>
2022-05-30 14:47:23 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -12,6 +12,28 @@ CREATE TABLE IF NOT EXISTS customers
   PRIMARY KEY(customer_id, preferred_region)
 ) PARTITION BY LIST (preferred_region);
 
+--
+CREATE TABLESPACE eu_central_1_tablespace WITH (
+  replica_placement='{"num_replicas": 3, "placement_blocks":
+  [{"cloud":"aws","region":"eu-central-1","zone":"eu-central-1a","min_num_replicas":1},
+  {"cloud":"aws","region":"eu-central-1","zone":"eu-central-1a","min_num_replicas":1},
+  {"cloud":"aws","region":"eu-central-1","zone":"eu-central-1a","min_num_replicas":1}]}'
+);
+
+CREATE TABLESPACE us_west_2_tablespace WITH (
+  replica_placement='{"num_replicas": 3, "placement_blocks":
+  [{"cloud":"aws","region":"us-west-2","zone":"us-west-2a","min_num_replicas":1},
+  {"cloud":"aws","region":"us-west-2","zone":"us-west-2a","min_num_replicas":1},
+  {"cloud":"aws","region":"us-west-2","zone":"us-west-2a","min_num_replicas":1}]}'
+);
+
+CREATE TABLESPACE ap_southeast_1_tablespace WITH (
+  replica_placement='{"num_replicas": 3, "placement_blocks":
+  [{"cloud":"aws","region":"ap-southeast-1","zone":"ap-southeast-1a","min_num_replicas":1},
+  {"cloud":"aws","region":"ap-southeast-1","zone":"ap-southeast-1a","min_num_replicas":1},
+  {"cloud":"aws","region":"ap-southeast-1","zone":"ap-southeast-1a","min_num_replicas":1}]}'
+);
+
 --create unique index if not exists uk_cpref_userid on user_preferences using lsm (customer_id,preferred_region);
 
 CREATE TABLE if not exists customer_us_west
